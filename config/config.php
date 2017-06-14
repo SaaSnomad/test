008a61d782fcf70c6f9ccf24f2817fd163a2640a
vovanest <vovanest@users.noreply.github.com>
2017-06-15 01:00:19 +0300

    webserver paths corrected

--- config/config.php
+++ config/config.php
@@ -1,11 +1,21 @@
 <?php
-//DB Params
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
-define('DB_NAME','saasnomad');
 
-define('SITE_TITLE', 'SaasNomad!');
+// DB Params
+   define('DB_HOST','localhost');
+   define('DB_USER','root');
+   define('DB_PASS','');
+   define('DB_NAME','saasnomad');
+
+   define('SITE_TITLE', 'SaaSnomad');
 
 // Paths
-//define ('BASE_URI', 'http://'.$_SERVER['SERVER_NAME'].'/saasnomad/test/‘);
+// define ('BASE_URI', 'http://test.saasnomad.com‘);
+
+
+// Define the webserver and path parameters
+   // Main webserver: eg, http://localhost - should not be empty for productive servers
+   // HTTP_SERVER is your Main webserver: eg, http://www.yourdomain.com
+   // HTTPS_SERVER is your Secure webserver: eg, https://www.yourdomain.com
+   define('HTTP_SERVER', 'http://test.saasnomad.com');
+   define('HTTPS_SERVER', 'https://test.saasnomad.com');
+?>
