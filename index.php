d9e279c07cff0d033e649e881f5f42bbc42ad7ed
Evgeniia Mishchenko <evgeniia.mishchenko@gmail.com>
2017-07-25 22:22:43 +0300

    Cool hoverable pricing table :)

--- index.php
+++ index.php
@@ -278,7 +278,7 @@
         <div class="row">
             <div class="col-lg-1 col-md-1 col-sm-1 hidden-xs"></div>
             <div class="col-lg-10 col-md-10 col-sm-10 col-xs-12">
-                        <table class="table" id="pricing-table">
+                        <table class="table table-hover" id="pricing-table">
                           <thead>
                             <tr>
                               <th></th>
