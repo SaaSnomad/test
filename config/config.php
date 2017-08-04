465c37765f283a944b9e743881ca170519c607f7
vovanest <vovanest@users.noreply.github.com>
2017-08-05 01:46:09 +0300

    minor fixes

--- config/config.php
+++ config/config.php
@@ -1,21 +1,19 @@
 <?php
 // DB Params
    define('DB_HOST','localhost');
+   //define('DB_USER','sndb28');
+   //define('DB_PASS','Saasnomad28');
    define('DB_USER','sndb28');
    define('DB_PASS','Saasnomad28');
-   //define('DB_USER','root');
-   //define('DB_PASS','');
    define('DB_NAME','saasnomad');
    define('SITE_TITLE', 'SaaSnomad');
 // Paths
-// define ('BASE_URI', 'https://test.saasnomad.com');
+   define ('BASE_URL', 'https://test.saasnomad.com');
+   define('ROOT_PATH', 'https://test.saasnomad.com');
 // Define the webserver and path parameters
    // Main webserver: eg, http://localhost - should not be empty for productive servers
    // HTTP_SERVER is your Main webserver: eg, http://www.yourdomain.com
    // HTTPS_SERVER is your Secure webserver: eg, https://www.yourdomain.com
-   define('HTTP_SERVER', 'http://test.saasnomad.com');
-   define('HTTPS_SERVER', 'https://test.saasnomad.com'); 
-
-   define("BASE_URL", "/test/");
-   define("ROOT_PATH", $_SERVER["DOCUMENT_ROOT"] . "/test/");
+   define('BASE_URL', '/test/');
+   define('ROOT_PATH', $_SERVER['DOCUMENT_ROOT'] . '/test/');
 ?>
