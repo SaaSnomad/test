9913b539ec43eab20e15c7167f29edbb30602738
vovanest <vovanest@users.noreply.github.com>
2017-08-05 02:14:43 +0300

    BASE_URL bug fix

--- config/config.php
+++ config/config.php
@@ -1,19 +1,21 @@
 <?php
-// DB Params
+// Database
    define('DB_HOST','localhost');
-   //define('DB_USER','sndb28');
-   //define('DB_PASS','Saasnomad28');
    define('DB_USER','sndb28');
    define('DB_PASS','Saasnomad28');
    define('DB_NAME','saasnomad');
    define('SITE_TITLE', 'SaaSnomad');
-// Paths
-   define ('BASE_URL', 'https://test.saasnomad.com');
-   define('ROOT_PATH', 'https://test.saasnomad.com');
-// Define the webserver and path parameters
+   
+   // Define the webserver and path parameters
    // Main webserver: eg, http://localhost - should not be empty for productive servers
    // HTTP_SERVER is your Main webserver: eg, http://www.yourdomain.com
    // HTTPS_SERVER is your Secure webserver: eg, https://www.yourdomain.com
-   define('BASE_URL', '/test/');
-   define('ROOT_PATH', $_SERVER['DOCUMENT_ROOT'] . '/test/');
+   
+// Servers
+   define('HTTP_SERVER', 'http://test.saasnomad.com');
+   define('HTTPS_SERVER', 'https://test.saasnomad.com');
+
+// Paths
+   define('BASE_URL', '');
+   define('ROOT_PATH', $_SERVER['DOCUMENT_ROOT'] . '');
 ?>
