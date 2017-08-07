f542bd65f31c53a37491d411cc6f4270ac691b9a
vovanest <vovanest@users.noreply.github.com>
2017-08-07 23:52:31 +0300

    file version conflict fix

--- config/config.php
+++ config/config.php
@@ -1,21 +1,21 @@
 <?php
-// DB Params
+// Database
    define('DB_HOST','localhost');
    define('DB_USER','sndb28');
    define('DB_PASS','Saasnomad28');
-   //define('DB_USER','root');
-   //define('DB_PASS','');
    define('DB_NAME','saasnomad');
    define('SITE_TITLE', 'SaaSnomad');
-// Paths
-// define ('BASE_URI', 'https://test.saasnomad.com');
-// Define the webserver and path parameters
+   
+   // Define the webserver and path parameters
    // Main webserver: eg, http://localhost - should not be empty for productive servers
    // HTTP_SERVER is your Main webserver: eg, http://www.yourdomain.com
    // HTTPS_SERVER is your Secure webserver: eg, https://www.yourdomain.com
+   
+// Servers
    define('HTTP_SERVER', 'http://test.saasnomad.com');
-   define('HTTPS_SERVER', 'https://test.saasnomad.com'); 
+   define('HTTPS_SERVER', 'https://test.saasnomad.com');
 
-   define("BASE_URL", "/test/");
-   define("ROOT_PATH", $_SERVER["DOCUMENT_ROOT"] . "/test/");
+// Paths
+   define('BASE_URL', '');
+   define('ROOT_PATH', $_SERVER['DOCUMENT_ROOT'] . '');
 ?>
